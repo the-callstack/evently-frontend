b41830798077cd921b5a28823c7df572dbe959eb
HodRZ <hodaifa.radwan@gmail.com>
2022-12-12 10:35:13 +0300

    feat: Add modal and layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import Footer from './components/Footer';
+import Layout from './components/Layout';
+import CookiesModal from './components/CookiesModal';
+import LoginWithSplash from './features/auth/LoginWithSplash';
 
 
 function App() {
   return (
-    <div className='App'>
-      <h1 className='font-mono text-5xl'>Hello World!</h1>
-    </div>
+    <>
+    <Layout/>
+    <LoginWithSplash/>
+     {/* <div className='App'>
+       <h1 className='font-mono text-5xl'>Hello World!</h1>
+    </div> */}
+    <CookiesModal/>
+    <Footer/>
+    </>
   );
 }
 
