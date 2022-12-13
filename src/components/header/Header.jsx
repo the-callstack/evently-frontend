20ef65f648c1726906ffe57ed21c9595112d3d8d
EmanRiziq <emanriziq@gmail.com>
2022-12-13 20:52:51 +0200

    refactor :sign in

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
+import { selectUserState } from '../../features/auth/authSlice';
 
 export default function Header() {
+  const { isLoggedIn,loggedUser} = useSelector(selectUserState);
+
   return (
     <>
 
@@ -19,7 +23,7 @@ export default function Header() {
           </li>
         </ul>  
         <div className="hidden xl:flex items-center space-x-5">
-        <a className="hover:text-gray-200 hover:underline" href="/auth">Sign in</a>
+        <a className="hover:text-gray-200 hover:underline" href="/auth">   {!isLoggedIn?"signIn": "signout"}     </a>
           <a className="hover:text-gray-200" href="#ss">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
