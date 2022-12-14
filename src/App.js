bcda5b5700ce80c3244db4abb3ea298801995918
HodRZ <hodaifa.radwan@gmail.com>
2022-12-14 19:58:01 +0300

    feat(package):add aggregator and Formik

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import CookiesModal from './components/CookiesModal';
 import LoginWithSplash from './features/auth/LoginWithSplash';
 import { Product } from './components/productDisplay/Product';
 import DetailsCard from './components/DetailsCard';
+import Aggregator from './components/aggregator/Aggregator';
 
 
 
@@ -12,12 +13,13 @@ import DetailsCard from './components/DetailsCard';
 function App() {
   return (
     <>
+      <Aggregator />
       <Product />
-      <Layout/>
-      <LoginWithSplash/>
-      <DetailsCard className ="pt-96" />
-      <CookiesModal/>
-      <Footer/>
+      <Layout />
+      <LoginWithSplash />
+      <DetailsCard className="pt-96" />
+      <CookiesModal />
+      <Footer />
     </>
   );
 }
--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import eventCatReducer from '../features/EventCat/EventCatSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    eventCat: eventCatReducer
   },
 });
