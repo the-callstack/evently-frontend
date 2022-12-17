587d33db785c0cc0527a9d174ad6988fcd2d1990
eyad-mansour <eyad.mansour.1997@gmail.com>
2022-12-17 15:38:16 +0300

    feat(update store update item)

--- src/components/provider/AddStore.jsx
+++ src/components/provider/AddStore.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const AddStore = () => {
     return (
         <>
-            <form className="w-full max-w-sm">
+            <form enctype='multipart/form-data' className="w-full max-w-sm">
   <div className="md:flex md:items-center mb-6">
     <div className="md:w-1/3">
       <label
@@ -19,6 +19,23 @@ export const AddStore = () => {
         id="inline-full-name"
         type="text"
         placeholder="7Eleven"
+      />
+    </div>
+  </div>
+  <div className="md:flex md:items-center mb-6">
+    <div className="md:w-1/3">
+      <label
+        className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+        htmlFor="inline-full-name"
+      >
+        Upload Logo
+      </label>
+    </div>
+    <div className="md:w-2/3">
+      <input
+        className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+        id="inline-full-name"
+        type="file"
       />
     </div>
   </div>
--- src/components/provider/Provider.jsx
+++ src/components/provider/Provider.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { AddItem } from "./AddItem";
 import { AddStore } from "./AddStore";
+import { UpdateItem } from "./UpdateItem";
+import { UpdateStore } from "./UpdateStore";
 
 export const Provider = () => {
     return (
         <>
             <AddStore />
             <AddItem />
+            <UpdateStore />
+            <UpdateItem />
         </>
     )
 }
