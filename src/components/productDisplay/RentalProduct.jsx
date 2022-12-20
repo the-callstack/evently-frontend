7cd0a9ac2f4c8bfd23c288e335b24ccec3083976
EmanRiziq <emanriziq@gmail.com>
2022-12-20 12:13:34 +0200

    fix:layout

--- src/components/productDisplay/RentalProduct.jsx
+++ src/components/productDisplay/RentalProduct.jsx
@@ -1,14 +1,14 @@
 import { FilterRental } from "../filter/FilterRental";
 import { ItemCard } from "../itemCard/ItemCard";
-import {useSelector, useDispatch} from 'react-redux';
-import {useEffect} from 'react'
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react'
 import { selectRentalItemsState } from "../../features/rentalItems/rentalItemsSlice";
 import { getAllRentals } from '../../features/rentalItems/rentalItemsSlice';
 import { Outlet } from "react-router-dom";
 import { getAllCategories, selectCategoryState } from "../../features/categories/categorySlice";
 export const RentalProduct = () => {
 
-  const  {rentalItems}  = useSelector(selectRentalItemsState);
+  const { rentalItems } = useSelector(selectRentalItemsState);
   const { category } = useSelector(selectCategoryState);
   const dispatch = useDispatch();
 
@@ -16,23 +16,28 @@ export const RentalProduct = () => {
     dispatch(getAllRentals());
     dispatch(getAllCategories())
 
-  },[]);
+  }, []);
   return (
     <div>
       <div className="flex justify-center gap-10 pt-10 pl-14">
-        <div>
-          <FilterRental category= {category.categories} /> 
+        <div className="pt-16">
+          <FilterRental category={category.categories} />
         </div>
-        <div className="grid grid-cols-3 gap-5 w-full  px-14 pt-4 pb-12 ">
-        {
-          
-          rentalItems.rentalItems.map(item=>{
-            return(
-            <ItemCard key={item.id} item={item}/>
-            )
-          })
-        }
-        <Outlet />
+        <div>
+          <div className="flex justify-center">
+            <h1 className="text-5xl pb-12 ">Rental Items</h1>
+          </div>
+          <div className="grid grid-cols-3 gap-5 w-full  px-14 pt-4 pb-12 ">
+            {
+
+              rentalItems.rentalItems.map(item => {
+                return (
+                  <ItemCard key={item.id} item={item} />
+                )
+              })
+            }
+            <Outlet />
+          </div>
         </div>
       </div>
     </div>
--- src/components/productDisplay/SelectProduct.jsx
+++ src/components/productDisplay/SelectProduct.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { TypeCard } from "./TypeCard";
-import img1 from '../../assets/sale2.png'
-import img2 from '../../assets/rent2.png'
+import img1 from '../../assets/for-rent-sign-real-estate-advertising-house-rent-vector-25856845.jpg'
+import img2 from '../../assets/13885334.jpg'
 export const SelectProduct = () => {
   const data = [
     {
       id: 1,
-      img: img2,
+      img: img1,
       title: 'Rent Here !',
       hash1: 'wedding',
       hash2: 'birthday',
@@ -15,7 +15,7 @@ export const SelectProduct = () => {
     },
     {
       id: 2,
-      img: img1,
+      img: img2,
       title: 'Buy Here !',
       hash1: 'graduation',
       hash2: 'birthday',
